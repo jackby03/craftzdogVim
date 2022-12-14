3a988ca5c581a6984042d67bfc996e2c902d4a57
jack <jackdelaguilavilla03@gmail.com>
2022-12-13 23:36:00 -0500

    new config 2.0

--- init.lua
+++ init.lua
@@ -1,3 +1,7 @@
+vim.defer_fn(function()
+  pcall(require, "impatient")
+end, 0)
+
 require('base')
 require('highlights')
 require('maps')
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -9,56 +9,56 @@ packer.startup(function(use)
   use("lewis6991/impatient.nvim")
 
   -- LSP
-	use({
-		"neovim/nvim-lspconfig",
-		"hrsh7th/cmp-nvim-lsp-signature-help",
-		"jose-elias-alvarez/null-ls.nvim",
-    "glepnir/lspsaga.nvim" -- LSP UIs
-	})
+  use({
+    "neovim/nvim-lspconfig",
+    "hrsh7th/cmp-nvim-lsp-signature-help",
+    "jose-elias-alvarez/null-ls.nvim",
+    "glepnir/lspsaga.nvim", -- LSP UIs
+  })
   -- Installation of LSP/Debuggers/Other
-	use({
+  use({
     "onsails/lspkind-nvim", -- vscode-like pictograms
-		"williamboman/mason.nvim",
-		"williamboman/mason-lspconfig.nvim",
-		"WhoIsSethDaniel/mason-tool-installer.nvim",
-	})
+    "williamboman/mason.nvim",
+    "williamboman/mason-lspconfig.nvim",
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+  })
 
   -- Completion
-	use({
+  use({
     "L3MON4D3/LuaSnip",
-		"hrsh7th/nvim-cmp",
-		requires = {
-			"hrsh7th/cmp-buffer",
-			"hrsh7th/cmp-path",
-			"hrsh7th/cmp-nvim-lua",
-			"hrsh7th/cmp-nvim-lsp",
-			--"hrsh7th/cmp-cmdline",
-			--"hrsh7th/cmp-git",
-			--"rcarriga/cmp-dap",
-			--"saadparwaiz1/cmp_luasnip",
-			--"onsails/lspkind-nvim",
-      --      {"L3MON4D3/LuaSnip", tag = "v1.*"},
-			"windwp/nvim-autopairs",
-		},
-	})
+    "hrsh7th/nvim-cmp",
+    requires = {
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+      "hrsh7th/cmp-nvim-lua",
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-cmdline",
+      "hrsh7th/cmp-git",
+      "rcarriga/cmp-dap",
+      "saadparwaiz1/cmp_luasnip",
+      "onsails/lspkind-nvim",
+      { "L3MON4D3/LuaSnip", tag = "v1.*" },
+      "windwp/nvim-autopairs",
+    },
+  })
 
   use({
-		"ThePrimeagen/refactoring.nvim",
-		requires = {
-			{ "nvim-lua/plenary.nvim" },
-			{ "nvim-treesitter/nvim-treesitter" },
-		},
-	})
+    "ThePrimeagen/refactoring.nvim",
+    requires = {
+      { "nvim-lua/plenary.nvim" },
+      { "nvim-treesitter/nvim-treesitter" },
+    },
+  })
 
-	use({
-		"nvim-treesitter/nvim-treesitter",
-		run = ":TSUpdate",
-		requires = {
-			"nvim-treesitter/playground",
-			"nvim-treesitter/nvim-treesitter-refactor",
-			"nvim-treesitter/nvim-treesitter-textobjects",
-		},
-	})
+  use({
+    "nvim-treesitter/nvim-treesitter",
+    run = ":TSUpdate",
+    requires = {
+      "nvim-treesitter/playground",
+      "nvim-treesitter/nvim-treesitter-refactor",
+      "nvim-treesitter/nvim-treesitter-textobjects",
+    },
+  })
 
   use "nvim-lualine/lualine.nvim" -- Statusline
   use 'norcalli/nvim-colorizer.lua'
@@ -72,10 +72,9 @@ packer.startup(function(use)
   --
   use {
     'nvim-telescope/telescope.nvim',
-    requires = {
-      'kyazdani42/nvim-web-devicons',-- File icons
-      'nvim-telescope/telescope-file-browser.nvim',
-    },
+    'kyazdani42/nvim-tree.lua',
+    'kyazdani42/nvim-web-devicons', -- File icons
+    'nvim-telescope/telescope-file-browser.nvim',
   }
 
 end)

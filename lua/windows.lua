d9a7405582a284bedd6bc3b21b353f4ee3b878b5
jackd <jackdelaguilavilla03@gmail.com>
2023-05-16 21:43:57 -0500

    fix os

--- lua/windows.lua
+++ lua/windows.lua
@@ -2,7 +2,9 @@ vim.opt.clipboard:prepend { 'unnamed', 'unnamedplus' }
 
 local packer_path = vim.fn.stdpath('data') .. " $env:LOCALAPPDATA\\nvim-data\\site\\pack\\packer\\start\\packer.nvim"
 
-if vim.fn.empty(vim.fn.glob(packer_path)) == 0 then
+if vim.fn.empty(vim.fn.glob(packer_path)) > 0 then
+  require('plugins')
+else
   vim.cmd(
     '!git clone https://github.com/wbthomas/packer.nvim ' .. packer_path)
   print("Packer Installed")
@@ -10,6 +12,4 @@ if vim.fn.empty(vim.fn.glob(packer_path)) == 0 then
   print("Loaded Plugins")
   vim.cmd("autocmd VimEnter * PackerSync")
   print("Downloading Packages")
-else
-  require('plugins')
 end

b606b1766c66211ade0ce5999e3177f2d63b2823
jackd <jackdelaguilavilla03@gmail.com>
2023-05-24 19:16:49 -0500

    fix flutter dependences linux

--- after/plugin/flutter-tools.rc.lua
+++ after/plugin/flutter-tools.rc.lua
@@ -1,5 +1,7 @@
 local status, flutter_tools = pcall(require, "flutter-tools")
 if not status then return end
+local st, telescope = pcall(require, "telescope")
+if (not st) then return end
 
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
 local on_attach = function(client, bufnr)
@@ -52,3 +54,6 @@ flutter_tools.setup {
 
 local keymap_set = vim.keymap.set
 keymap_set('n', 'ff', ':Telescope flutter commands<CR>')
+
+
+telescope.load_extension("flutter")
